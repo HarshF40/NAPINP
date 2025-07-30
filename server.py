d6e2e026f51e6e13232b3b61ff05d09785c9e085
HarshF40 <harshgaonkar57@gmail.com>
2025-07-30 08:06:04 +0530

    Add image support

--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 # Misuse of such scripts can lead to account bans or legal consequences.
 # The author is not responsible for any misuse or damage caused by this script.
 # Only works with one site
+# Version 2.0; Image Support
 
 import time
 from dotenv import load_dotenv
@@ -26,7 +27,7 @@ def init_page():
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch_persistent_context(
         user_data_dir=os.getenv("USER_DATA_DIR"),
-        headless=True,
+        headless=False,
         args=[
             "--disable-blink-features=AutomationControlled",
             "--start-maximized",
@@ -71,8 +72,9 @@ def receive_request():
     time.sleep(1)
 
     # Enter the query and send
-    textbox.type(query, delay=80)
-    time.sleep(1)
+    #textbox.type(query, delay=10)
+    textbox.fill(query)
+    time.sleep(2)
     textbox.press("Enter")
     time.sleep(2)
     
@@ -83,11 +85,65 @@ def receive_request():
 
     # Locate the Response content
     locater = page.locator('xpath=(//div[contains(@class, "text-input-field")])[1]/preceding::message-content[1]')
-    locater.wait_for(timeout=10000)
+    locater.wait_for(timeout=10000000)
 
     # Extract the content
     response = locater.inner_text()
     return jsonify({"status": "success", "response":response})
+
+@app.route('/img', methods=['POST'])
+def receive_img():
+    global playwright, browser, page 
+    data = request.get_json(force=True)
+    query = data.get("query")
+    links = data.get("links")
+    # Locate the input field
+    textbox = page.get_by_role("textbox", name="Enter a prompt here")
+    textbox.click()
+    time.sleep(1)
+
+    textbox.fill(query)
+    time.sleep(2)
+
+
+    upload_button = page.locator('button[aria-label="Open upload file menu"].upload-card-button.open')
+    upload_button.wait_for(timeout=5000)
+    upload_button.click()
+    time.sleep(1)
+
+    upload_files_menu_item = page.locator("div.menu-text", has_text="Upload files")
+    upload_files_menu_item.wait_for(timeout=5000)
+    upload_files_menu_item.click()
+    time.sleep(1)
+
+    import subprocess
+    subprocess.run('hyprctl dispatch closewindow address:$(hyprctl clients -j | jq -r \'.[] | select(.class=="Chrome" and .title=="Open Files") | .address\')',
+        shell=True,
+        check=True
+    )
+    time.sleep(3)
+    file_input = page.locator('input[type="file"]')
+    #file_input.wait_for(state="attached", timeout=10000)
+    file_input.set_input_files(links)
+    print("✅ Image uploaded successfully.") 
+    time.sleep(25) # Statically wait for the image to uplaod
+    textbox.click()
+    textbox.press("Enter")
+    time.sleep(2)
+    
+    # Locate the closest user query bubble
+    # Checking if content is loaded completely
+    # Wait for the spinner below that specific query bubble to be marked as completed
+    page.wait_for_selector('.text-input-field >> xpath=preceding::span[contains(@class, "user-query-bubble-with-background")][1]/following::div[@data-test-lottie-animation-status="completed"][1]', timeout=6000000)
+
+    # Locate the Response content
+    locater = page.locator('xpath=(//div[contains(@class, "text-input-field")])[1]/preceding::message-content[1]')
+    locater.wait_for(timeout=10000000)
+
+    # Extract the content
+    response = locater.inner_text()
+    return jsonify({"status": "success", "response":response})
+
 
 if __name__ == "__main__":
     init_page()
